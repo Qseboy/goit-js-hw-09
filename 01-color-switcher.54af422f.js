8631e67b4e06b298fe737c0785b121e2b5a59a1b
Qseboy <42722431+Qseboy@users.noreply.github.com>
2023-06-09 16:04:22 +0000

    Deploying to gh-pages from @ Qseboy/goit-js-hw-09@ed79b8534c008b4e8f5850a1e08d9bba09896c14 🚀

---
+++ 01-color-switcher.54af422f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){n&&(clearInterval(n),e.setAttribute("disabled",!0),t.removeAttribute("disabled"))}))}();
+//# sourceMappingURL=01-color-switcher.54af422f.js.map
